a06e88116a1fb8038ace50f7de5078d9a8c79daf
jackyheart <jacky.coolheart@gmail.com>
2016-07-31 19:43:25 +0800

    move to github

---
+++ Direction/MapAnnotation.h
@@ -0,0 +1,16 @@
+//
+//  MapAnnotation.h
+//  Direction
+//
+//  Created by Jacky on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MapAnnotation : NSObject {
+    
+}
+
+@end
---
+++ Direction/MapAnnotation.m
@@ -0,0 +1,14 @@
+//
+//  MapAnnotation.m
+//  Direction
+//
+//  Created by Jacky on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapAnnotation.h"
+
+
+@implementation MapAnnotation
+
+@end
